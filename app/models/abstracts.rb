85bcd284b4557bb4119f7864d71b80002938830b
obfuscurity <jason@dixongroup.net>
2014-02-01 16:37:55 -0500

    add methods for finding scored/unscored lists of abstracts

--- app/models/abstracts.rb
+++ app/models/abstracts.rb
@@ -22,9 +22,24 @@ class Abstract < Sequel::Model
 
   def self.fetch_abstracts_with_related
     Abstract.select('abstracts.*'.lit, 'speakers.full_name AS speaker'.lit, :speakers__email, 'events.name AS event_name'.lit).
-      from(:abstracts, :speakers, :events).
+      from(:abstracts, :speakers, :events, :scores).
       where(:abstracts__speaker_id => :speakers__id).
       where(:abstracts__event_id => :events__id).
-      order(:abstracts__id).all
+      order(:abstracts__id)
+  end
+
+  def self.fetch_unscored_by_user(user)
+    Abstract.fetch_abstracts_with_related.
+      exclude(:scores__abstract_id => :abstracts__id, :scores__judge => user)
+  end
+
+  def self.fetch_scored_by_user(user)
+    Abstract.fetch_abstracts_with_related.
+      where(:scores__abstract_id => :abstracts__id, :scores__judge => user)
+  end
+
+  def self.fetch_random_unscored_by_user(user)
+    @abstracts = Abstract.fetch_unscored_by_user(user).all
+    return @abstracts[rand(@abstracts.count).to_int]
   end
 end
