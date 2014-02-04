93a3d89dfaf61ace89f2ff14b1ad17e3cdcae1db
obfuscurity <jason@dixongroup.net>
2014-02-03 22:30:49 -0500

    class for creating donut charts

---
+++ app/public/js/chart_donut.js
@@ -0,0 +1,31 @@
+
+var Donut = function(args) {
+
+  var element = args.element;
+  var dataset = args.dataset;
+  var width = args.width || 100;
+  var height = args.height || 100;
+  var radius = Math.min(width, height) / 2;
+  var colors = args.colors;
+
+  if (typeof colors == 'undefined') {
+    var color = d3.scale.category20();
+  } else {
+    var color = d3.scale.ordinal().range(colors);
+  }
+
+  var pie = d3.layout.pie().sort(null);
+  var arc = d3.svg.arc().innerRadius(radius - (width / 3)).outerRadius(radius - width / 6);
+
+  var svg = d3.select(element).append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+  var path = svg.selectAll("path")
+    .data(pie(dataset))
+    .enter().append("path")
+    .attr("fill", function(d, i) { return color(i); })
+    .attr("d", arc);
+};
