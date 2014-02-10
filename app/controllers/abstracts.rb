b16b951f1b4ddd9cc17fe9d472435fe340a33dea
obfuscurity <jason@dixongroup.net>
2014-02-10 13:45:04 -0500

    pass sort param to model

--- app/controllers/abstracts.rb
+++ app/controllers/abstracts.rb
@@ -6,7 +6,7 @@ module Judy
   class Web < Sinatra::Base
 
     get '/abstracts/?' do
-      @abstracts = Abstract.fetch_all_abstracts_and_scores(:judge => session[:user])
+      @abstracts = Abstract.fetch_all_abstracts_and_scores(:judge => session[:user], :sort => params[:sort])
       status 200
       erb :'abstracts/index', :locals => {
         :abstracts => @abstracts,
